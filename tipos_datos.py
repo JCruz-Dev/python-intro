3061e08313f00a068165dca16adfe9c536330ccd
Juan carlos <juancarloscruz278@gmail.com>
2020-05-24 21:40:18 -0400

    Variables and data types in python

---
+++ tipos_datos.py
@@ -0,0 +1,10 @@
+nombre = "juan carlos"
+edad = 28
+cantidad_apagar = 100.20
+
+print(nombre, edad, cantidad_apagar)
+
+# Los booleans se escriben con la primera palabra mayuscula
+
+verdadero = True
+print(verdadero)
---
+++ variables.py
@@ -0,0 +1,10 @@
+nombre = "juan"
+clientNumber = "123213"
+# En python es recomendado snake case para las variables
+client_hello = "asdasdasd"
+
+favorite_book = "The bible"
+last_meal = "habichuelas con dulce"
+
+print(favorite_book)
+print(last_meal)
