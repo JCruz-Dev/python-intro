81b91eb2a2ca26524156f43a30bb20c011046f52
Juan carlos <juancarloscruz278@gmail.com>
2020-05-24 22:10:14 -0400

    Operators and functions with numbers

---
+++ operadores-funciones.py
@@ -0,0 +1,24 @@
+def suma(a, b):
+    print('Suma', a + b)
+
+
+def resta(a, b):
+    print('Resta', a - b)
+
+
+def multiplicar(a, b):
+    print('Multiplicacion', a * b)
+
+
+def dividir(a, b):
+    # int permite tomar la parte real de un decimal
+    print('Division', int(a / b))
+
+
+def potenciar(a, b):
+    print('Potencia', a ** b)
+
+
+suma(2, 2)
+potenciar(4, 3)
+dividir(10, 2)
